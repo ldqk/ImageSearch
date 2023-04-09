6a94947ac66e839e227e151160d8b662fb681074
懒得勤快 <admin@masuit.com>
2023-04-09 21:44:36 +0800

    修正选择文件夹卡死的问题

--- 以图搜图/Program.cs
+++ 以图搜图/Program.cs
@@ -1,8 +1,19 @@
 using Masuit.Tools.Logging;
 using System.Diagnostics;
-using 以图搜图;
 
-Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.BelowNormal;
-AppDomain.CurrentDomain.UnhandledException += (sender, e) => LogManager.Error((Exception)e.ExceptionObject);
-ApplicationConfiguration.Initialize();
-Application.Run(new Form1());
+namespace 以图搜图;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    private static void Main()
+    {
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.BelowNormal;
+        AppDomain.CurrentDomain.UnhandledException += (sender, e) => LogManager.Error((Exception)e.ExceptionObject);
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
